2e0dc7b45d71f7790da8f9ba8ac98929ca280e1d
TheDoudou <thechikan@mac.com>
2018-10-25 14:33:48 +0200

    Update api php
    Update for add user in array and push down list

--- assets/lib/thedoudou/pioupiou_file_gestion.php
+++ assets/lib/thedoudou/pioupiou_file_gestion.php
@@ -1,13 +1,15 @@
 <?php
-if(!empty($_POST['name'])){
-    $name = $_POST['name'];
-    $score = $_POST['score'];
+if(!empty($_GET['name']) && $_GET['score'] <= 110) {
+    $name = $_GET['name'];
+    $score = $_GET['score'];
     $file = file_get_contents('../../data/pioupiou_db.json');
     $json_decode = json_decode($file, true);
     for ($i = 0; $i < count($json_decode); $i++) {
         if($score > $json_decode[$i]['score']) {
-            $json_decode[$i]['name'] = $name;
-            $json_decode[$i]['score'] = $score;
+            $data[$i]['name'] = $name;
+            $data[$i]['score'] = $score;
+            array_splice( $json_decode, $i, 0, $data );
+            array_pop($json_decode);
             break;
         }
     }
@@ -15,5 +17,7 @@ if(!empty($_POST['name'])){
     $file = fopen('../../data/pioupiou_db.json', 'w');
     fwrite($file, $json_encode);
     fclose($file);
+
+    print_r($json_encode);
 }
 ?>
