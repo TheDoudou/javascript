537328bfc851c380cc8878a7f7a3b17cda0b37c5
TheDoudou <thechikan@mac.com>
2018-10-25 16:45:25 +0200

    Add protection
    Add protection for php api score (HTTP_REFERER)

--- assets/lib/thedoudou/pioupiou_file_gestion.php
+++ assets/lib/thedoudou/pioupiou_file_gestion.php
@@ -1,25 +1,27 @@
 <?php
-if(!empty($_POST['name']) && $_POST['score'] <= 110) {
+if(!empty($_POST['name']) && $_POST['score'] <= 110 && $_SERVER['HTTP_REFERER'] == "http://thedoudou.myds.me/be_code/javascript/pioupiou.php") {
     $name = $_POST['name'];
     $score = $_POST['score'];
-    $l = strlen($name);
-    if ($l > 15)
-        $name = substr($name, 0, -($l-14));
-        
-    $file = file_get_contents('../../data/pioupiou_db.json');
-    $json_decode = json_decode($file, true);
-    for ($i = 0; $i < count($json_decode); $i++) {
-        if($score > $json_decode[$i]['score']) {
-            $data[$i]['name'] = $name;
-            $data[$i]['score'] = $score;
-            array_splice( $json_decode, $i, 0, $data );
-            array_pop($json_decode);
-            break;
+    if(substr($_POST['score'], -1) == 0) {
+        $l = strlen($name);
+        if ($l > 15)
+            $name = substr($name, 0, -($l-14));
+
+        $file = file_get_contents('../../data/pioupiou_db.json');
+        $json_decode = json_decode($file, true);
+        for ($i = 0; $i < count($json_decode); $i++) {
+            if($score > $json_decode[$i]['score']) {
+                $data[$i]['name'] = $name;
+                $data[$i]['score'] = $score;
+                array_splice( $json_decode, $i, 0, $data );
+                array_pop($json_decode);
+                break;
+            }
         }
+        $json_encode = json_encode($json_decode);
+        $file = fopen('../../data/pioupiou_db.json', 'w');
+        fwrite($file, $json_encode);
+        fclose($file);
     }
-    $json_encode = json_encode($json_decode);
-    $file = fopen('../../data/pioupiou_db.json', 'w');
-    fwrite($file, $json_encode);
-    fclose($file);
 }
 ?>
